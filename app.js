76101014e77c8eeec65329fb32c407b2422965ef
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-08-28 03:30:38 +0300

    adding pug templating engine in our project

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const path = require("path");
 
 const server = http.createServer(app);
 
+app.set("views engine", "pug"); //This line sets the view engine to "pug". The view engine is responsible for rendering dynamic templates into HTML. By setting it to "pug," you're telling Express to use the Pug template engine for rendering views.
+app.set("views", "views"); //This line sets the directory where your template files (Pug templates) are located. The second argument, "views," is the path to the directory where your template files are stored. This directory path is relative to the root of your application.
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
